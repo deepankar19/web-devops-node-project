38dd561d562acde7a9d98f6f77468d2cb32188fb
deepankar dey <deepankardey12@gmail.com>
2024-06-24 12:04:26 +0530

    ci-cd modify

--- .terraform/providers.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-provider "kubernetes" {
-  host                   = aws_eks_cluster.demo.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.demo.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.demo.token
-}
